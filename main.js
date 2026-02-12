91b1563685a66a98e43c43d6849d309b67b7fcb8
Moeenc137 <moeenc137@gmail.com>
2026-02-12 18:07:45 +0500

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,14 @@ const renderData = function (data) {
   countriesContainter.insertAdjacentHTML("beforeend", html);
 };
 
+const getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 //fetching api using async/await the main difference between promises and this is that there are no
 //callbacks here, we use fetch just like in promises and same here we build promise but to handle the
 // promise we enclose all that using await function , await function just wait till the data is being
@@ -25,11 +33,47 @@ const getCountry = async function (country) {
     const req = await fetch(
       `https://countries-api-836d.onrender.com/countries/name/${country}`,
     );
-    const request = await req.json();
-    console.log(request);
-    renderData(request[0]);
+    const data = await req.json();
+    renderData(data[0]);
+    return `This is bordering country ${data[0].flag}`;
   } catch (err) {
     console.error(`Eror007${err}`); //error argument is default property given by try catch. err.message ,default property used for just displaying message.
   } //error handling using try and catch
 };
 getCountry("pakistan");
+// returning values from async function.
+
+// getCountry("bangladesh")
+//   .then((d) => console.log(d))
+//   .catch((err) => console.error(`your error ${err}`))
+//   .finally(() => console.log("run"))
+//writing async function to return same value.
+
+(async function () {
+  try {
+    const prmc = await getCountry("bangladesh");
+    console.log(prmc);
+  } catch (err) {
+    console.error(`your error ${err}`);
+  }
+  console.log("run");
+})();
+
+const get3countries = async function (c1, c2, c3) {
+  try {
+    const [data1] = getJSON(
+      `https://countries-api-836d.onrender.com/countries/name/${c1}`,
+    );
+    const [data2] = getJSON(
+      `https://countries-api-836d.onrender.com/countries/name/${c2}`,
+    );
+    const [data3] = getJSON(
+      `https://countries-api-836d.onrender.com/countries/name/${c3}`,
+    );
+    console.log(data1.capital, data2.capital, data3.capital);
+  } catch (err) {
+    console.error(`137 error${err}`);
+  }
+};
+
+get3countries("pakistan", "iran", "china");
